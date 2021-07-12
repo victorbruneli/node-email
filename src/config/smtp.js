dd84c9b38200318825c0a0fbc640bb96f92a5c9e
victorbruneli <vic.bruneli@hotmail.com>
2021-07-12 10:39:47 -0300

    First Commit

---
+++ src/config/smtp.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host: "smtp.gmail.com",
+  port: 587,
+  user: "Email para envio @gmail.com",
+  pass: "Senha do E-mail",
+};
---
+++ src/index.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+
+const SMTP_CONFIG = require("./config/smtp");
+
+const transporter = nodemailer.createTransport({
+  host: SMTP_CONFIG.host,
+  port: SMTP_CONFIG.port,
+  secure: false,
+  auth: {
+    user: SMTP_CONFIG.user,
+    pass: SMTP_CONFIG.pass,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+async function run() {
+  const mailSent = await transporter.sendMail({
+    text: "Corpo do texto e-mail ",
+    subject: "Assunto e-mail",
+    from: "VITAO DaMassa <testevitaodamassa@gmail.com>",
+    to: [
+      "testevitaodamassa@hotmail.com",
+      "teste41@exemplo.com.br",
+      "teste42@exemplo.com.br",
+      "teste43@exemplo.com.br",
+      "teste44@exemplo.com.br",
+      "teste45@exemplo.com.br",
+      "teste46@exemplo.com.br",
+    ],
+  });
+
+  console.log(mailSent);
+}
+
+run();
